ccb279520f079e4d789f8152c29134f3c8adde57
makshoop <makshoope@gmail.com>
2023-10-31 15:44:33 +0300

    end

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import AppHeader from './header/AppHeader';
 import BurgerConstructor from './burger-constructor/BurgerConstructor.jsx';
+import ProductList from './burger-ingredients/BurgerIngredients';
+import './index.css'
 
 function App() {
     return (
-        <div className="app">
+        <>
             <AppHeader />
-            
-                <BurgerConstructor />
-            
-        </div>
+            <div className="app">
+            <ProductList />
+            <BurgerConstructor />
+            </div>
+        </>
     );
 }
 
